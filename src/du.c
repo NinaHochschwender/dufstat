c7238a9a52d05ef1b5b5566cf08edd4deedd4eb3
NilsHochschwender <wetw55@gmail.com>
2018-04-27 21:17:55 +0200

    another safety pull;sry

---
+++ src/du.c
@@ -0,0 +1,20 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  du.c
+ *
+ *    Description:  the main algorithm for reading the dirs and writing the output to 
+ *                  a file for later usage by the other functions
+ *
+ *        Version:  1.0
+ *        Created:  27.04.2018 21:15:55
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Nils Hochschwender 
+ *   Organization:  None
+ *
+ * =====================================================================================
+ */
+#include <stdlib.h>
+#include "du.h"
