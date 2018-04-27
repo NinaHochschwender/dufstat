22543208802a34b05107110e815687e490450c00
NilsHochschwender <wetw55@gmail.com>
2018-04-27 18:40:24 +0200

    first sources to git. yeah

---
+++ src/du_s.c
@@ -0,0 +1,23 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  du_s.c
+ *
+ *    Description:  the sources for summarizing du
+ *
+ *        Version:  1.0
+ *        Created:  27.04.2018 18:27:00
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Nils Hochschwender
+ *   Organization:  None
+ *
+ * =====================================================================================
+ */
+#include "dus.h"
+#include <stdio.h>
+#include <sys/types.h>
+#include <dirent.h>
+
+
---
+++ src/du_s.h
@@ -0,0 +1,21 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  du_s.h
+ *
+ *    Description:  dusummarizing
+ *
+ *        Version:  1.0
+ *        Created:  27.04.2018 18:22:09
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  NilsHochschwender
+ *   Organization:  None
+ *
+ * =====================================================================================
+ */
+#ifndef DU_S_H
+	#define DU_S_H
+	
+#endif
