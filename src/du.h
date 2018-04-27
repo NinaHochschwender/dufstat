0485ea633585ddd085f2541c29125a1a69043484
NilsHochschwender <wetw55@gmail.com>
2018-04-27 21:23:33 +0200

    yet another useless push;sry

---
+++ src/du.h
@@ -0,0 +1,24 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  du.h
+ *
+ *    Description:  the main algorithm for reading the dirs and writing the output to 
+ *    		    a file for later usage by the other functions
+ *
+ *        Version:  1.0
+ *        Created:  27.04.2018 18:43:44
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Nils Hochschwender
+ *   Organization:  None
+ *
+ * =====================================================================================
+ */
+
+#ifndef DU_H
+	#define DU_H
+	
+	
+#endif
